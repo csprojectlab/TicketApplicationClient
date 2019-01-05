2437cb50903574e4834f1fb79642b141a9d6ec98
csprojectlab <aridaman_nandan@outlook.com>
2019-01-05 11:38:49 +0530

    ADDED VUE-COOKIE AND VUETIFY

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueCookie from 'vue-cookie'
+import Vuetify from 'vuetify'
+import('../node_modules/vuetify/dist/vuetify')
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+// import('../node_modules/vuetify/dist/vuetify.min.css')
 
+Vue.use(VueCookie)
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

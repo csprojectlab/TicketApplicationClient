a7ba94d4feb8c79167ab8137331af5fdcf9414ab
csprojectlab <aridaman_nandan@outlook.com>
2019-01-07 12:01:16 +0530

    REGISTER MODEL COMPLETE

--- src/components/authenticate/Authentication.vue
+++ src/components/authenticate/Authentication.vue
@@ -60,26 +60,27 @@
                     </v-toolbar>
                     <v-card-text>
                         <v-form>
-                            <v-text-field label="username" prepend-icon="face"
-                                :rules="['Required field']"></v-text-field>
-                            <v-text-field label="secret" prepend-icon="lock"
-                                :rules="['Required field']"></v-text-field>
-                            <v-text-field label="email" prepend-icon="email"
-                                :rules="['Required field']"></v-text-field>
+                            <v-text-field label="username" prepend-icon="face" type="text" v-model="regData.username"
+                                :rules="[() => regData.username.length > 0 || 'Required field']"></v-text-field>
+                            <v-text-field label="secret" prepend-icon="lock" type="password" v-model="regData.password"
+                                :rules="[() => regData.password.length > 0 || 'Required field']"></v-text-field>
+                            <v-text-field label="email" prepend-icon="email" type="email" v-model="regData.email"
+                                :rules="[() => regData.email.length > 0 || 'Required field']"></v-text-field>
                         </v-form>
                     </v-card-text>
                     <v-card-actions>
-                        <v-btn dark @click="registerModel=false" color="error">
+                        <v-btn dark @click="registerModel=false;regData.username='';regData.password='';regData.email=''" color="error">
                             <v-icon dark left>arrow_back</v-icon>Back
                         </v-btn>
                         <v-spacer></v-spacer>
-                        <v-btn dark color="pink accent-3">
+                        <v-btn dark color="pink accent-3" @click="submitRegistration()">
                             <v-icon dark left>add_circle</v-icon>Register
                         </v-btn>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
         </div>
+        <!-- END OF REGISTER MODEL -->
         <v-snackbar v-model="snackbar" color="error" bottom>
             {{message}}
         </v-snackbar>
@@ -92,7 +93,8 @@ export default {
             registerModel: false,
             message: '',
             snackbar: false,
-            credentials: {username: '', password: ''}
+            credentials: {username: '', password: ''},
+            regData: { username: '', password: '', email: ''}
         }
     },
     methods: {
@@ -103,7 +105,24 @@ export default {
                 return;
             }
             
+        },
+
+        submitRegistration () {
+            if(this.regData.username == '' || this.regData.password == '' || this.regData.email == '') {
+                this.snackbar = true;
+                this.message = "PROVIDE COMPLETE REGISTRATION CREDENTIALS."
+                return;
+            }
+            let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if(!re.test(this.regData.email)) {
+                this.snackbar = true;
+                this.message = "PROVIDED EMAIL IS NOT VALID."
+                return;
+            }
+            console.log(this.regData.username, this.regData.password, this.regData.email)
         }
+
+
     }
 }
 </script>
